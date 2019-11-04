9ed3c191d5fbf0c59b061ab252679367fe9ef8df
daryaU <daryashavzova@gmail.com>
2019-11-05 00:06:45 +0300

    Full project Front-End-Test with mobile adaptation

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var btn = document.querySelector('.burger');
+btn.onclick = function (e) {
+    var element = document.querySelector(".show-burger");
+    if (element.classList.contains("show-block")) {
+        element.classList.remove("show-block");
+    } else {
+        element.classList.add("show-block");
+    }
+}
