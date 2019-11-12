3103696d5834ba22cc7a150c4c97291a5e122fe4
daryaU <daryashavzova@gmail.com>
2019-11-13 01:21:15 +0300

    change file with scripts

--- js/main.js
+++ js/main.js
@@ -5,32 +5,34 @@ let notifications = document.getElementById("notifications");
 let arrPhoto = document.querySelectorAll(".wrapper-post__item__photos .wrapper-post__item__photos_img");
 let arrayElement = [];
 
+burger();
+tasks();
+countNotifications();
 
 // burger-menu
-document.querySelector('.burger').addEventListener('click', () => {  burger();
-});
-
 function burger() {
-    (element.classList.contains("show-block")) ? element.classList.remove("show-block") :
-        element.classList.add("show-block");
+    document.querySelector('.burger').addEventListener('click', () => {
+        element.classList.contains("show-block") ? element.classList.remove("show-block") :
+            element.classList.add("show-block");
+    });
 };
 
 // function for closed task
-document.querySelector('.wrap__left-menu__tasks_completed-tasks')
-    .addEventListener('click', () => {
-        tasks();
-    });
-
 function tasks() {
-    confirm("Are you sure you want to change the number of tasks?") ?(openTasks.innerHTML > 0 ? 
-    (completedTasks.innerHTML++ , openTasks.innerHTML--) :alert("Sorry! You don't have open tasks.")) : null;
+    document.querySelector('.wrap__left-menu__tasks_completed-tasks')
+        .addEventListener('click', () => {
+            confirm("Are you sure you want to change the number of tasks?") ? openTasks.innerHTML > 0 ?
+                (completedTasks.innerHTML++ , openTasks.innerHTML--) : alert("Sorry! You don't have open tasks.") : null;
+        });
 };
 
 // count notifications
-for (i = 0; i < arrPhoto.length; i++) {
-    arrayElement.push(arrPhoto[i]);
-    arrPhoto[i].addEventListener('click', function (e) {
-        notifications.innerHTML = arrayElement.indexOf(e.target);
-    });
-}
+function countNotifications() {
+    for (i = 0; i < arrPhoto.length; i++) {
+        arrayElement.push(arrPhoto[i]);
+        arrPhoto[i].addEventListener('click', (e) => {
+            notifications.innerHTML = arrayElement.indexOf(e.target);
+        });
+    }
 
+}
