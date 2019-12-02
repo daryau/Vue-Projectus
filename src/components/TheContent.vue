6d709d703ecfc1230a27b0e5416c8ebb25c5f3c9
daryaU <daryashavzova@gmail.com>
2019-12-03 00:21:47 +0300

    Transferring menu from TheHeader component to TheContentn

--- src/components/TheContent.vue
+++ src/components/TheContent.vue
@@ -1,5 +1,33 @@
 <template>
-    <div class="content-body"><router-view></router-view></div>
+    <div>
+      <nav class="nav">
+        <ul class="menu-list">
+          <li class="menu-list__item">
+              <router-link class="f16 menu-list__item__link"
+              exact to="/Tasks" active-class="is-active">
+                  Tasks
+              </router-link>
+          </li>
+          <li class="menu-list__item">
+              <a href="#" class="f16 menu-list__item__link">Kanban</a>
+          </li>
+          <li class="menu-list__item">
+              <router-link class="f16 menu-list__item__link" to="/Activity"
+                          active-class="is-active">Activity
+              </router-link>
+          </li>
+          <li class="menu-list__item">
+              <a href="#" class="f16 menu-list__item__link">Calendar</a>
+          </li>
+          <li class="menu-list__item">
+              <a href="#" class="f16 menu-list__item__link">Files</a>
+          </li>
+        </ul>
+      </nav>
+      <div class="content-body">
+        <router-view></router-view>
+      </div>
+      </div>
 </template>
 
 <script>
